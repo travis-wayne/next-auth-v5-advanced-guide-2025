f1f4ed418977f2bc420215ffa3220f89d81b1964
Travis Wayne <joshuaxp9@gmail.com>
2024-05-30 15:51:06 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { auth } from "@/auth";
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/sonner";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
